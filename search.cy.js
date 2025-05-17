f3c597f6e6714398cee52558596c328a341fa0cd
Rikkish <rikkish0807@gmail.com>
2025-05-17 09:27:37 +0530

    Update search.cy.js

--- search.cy.js
+++ search.cy.js
@@ -1,10 +1,10 @@
-describe('homepage', () => {
-    it('submodules', () => {
-       
+describe('search functionality', () => {
+    it('search', () => {
         
-        cy.visit("https://bewakoooff.netlify.app/html/")
+cy.visit("https://bewakoooff.netlify.app/html/")
+cy.get('[type="text"]').type('jogger')
+cy.get('[id="serachicon"]').click()
+cy.get('[id="product-path-title"]').should("be.visible")
 
-      cy.get('#navEnd span').contains("ACCESSORIES").click()
-        
     });
 });
